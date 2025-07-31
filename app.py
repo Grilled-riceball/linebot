f004572572300e1898dbe80a069bd28567ad4e44
kenta shimizu <shimizu-kxa@coay-planning.com>
2025-07-31 16:19:20 +0900

    その３:情報検索理論(前編)

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from linebot.v3.messaging import (
 from linebot.v3.webhooks import (
 	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
 )
-import os
+import os, random, re
+import pandas as pd
 
 ## .env ファイル読み込み
 from dotenv import load_dotenv
@@ -19,6 +20,82 @@ load_dotenv()
 ## 環境変数を変数に割り当て
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+
+## データ読み込み
+DATA = pd.read_csv('data/beatles.csv', index_col='song') ## 曲名をプライマリキーに設定
+SONGS = DATA.index  ## 曲名のリスト
+
+## 使用関数もろもろ
+def levenshtein(str1, str2):
+	str1 = '^' + str1
+	str2 = '^' + str2
+	distances = [list(range(len(str2)))]
+	for i in range(1, len(str1)):
+		row = [i]
+		for j in range(1, len(str2)):
+			insert = row[j-1] + 1
+			delete = distances[i-1][j] + 1
+			replace = distances[i-1][j-1] + int(str1[i]!=str2[j])
+			row.append(min(insert, delete, replace))
+		distances.append(row)
+	return distances[-1][-1]
+
+## function to tokenize text
+def tokenize(text:str) -> set:
+	rough_tokens = re.split(r'[\s;:,\-\(\)\"!?]', text.lower())
+	tokens = set()
+	for token in rough_tokens:
+		if token == '':
+			continue
+		if token.endswith("\'s"):
+			tokens.add(token.replace("\'s", ''))  ## day's - > day
+		if "\'" in token:
+			tokens.add(token.replace("\'", ''))  ## don't -> dont
+		tokens.add(token)
+	return tokens
+
+## function to remove whitespaces/punctuations
+## e.g. A Hard Day's Night -> aharddaysnight
+def remove_punct(text:str) -> str:
+	return ''.join(c for c in text if c.isalnum()).lower()
+
+INVERTED_INDEX_SONGTITLE = {}
+for song in SONGS:
+    for token in tokenize(song):
+        INVERTED_INDEX_SONGTITLE[token] = INVERTED_INDEX_SONGTITLE.get(token, set()) | {song}
+        
+def search_song_title(query:str) -> str:
+	query_lower = re.sub(r'\s\s+', ' ', query).strip().lower()  ## shrink space & lowercase 
+	query_no_punct = remove_punct(query_lower)  ## a hard day's -> aharddays
+	query_tokens = query_lower.split(' ')
+
+	partial_match = []
+
+	for song in SONGS:
+		song_lower = song.lower()  ## lowercase song title
+		song_no_punct = remove_punct(song)  ## remove punct 
+
+		### 1. EXACT MATCH ###
+		if query_lower==song_lower or query_no_punct==song_no_punct:
+			return song
+		
+		### 2. PARTIAL MATCH ###
+		if query_lower in song_lower or query_no_punct in song_no_punct:
+			partial_match.append(song)
+
+    	### 3. TOKEN MATCH ###
+		song_intersection = set(SONGS)
+		for token in query_tokens:
+			song_intersection &= INVERTED_INDEX_SONGTITLE.get(token, set())
+
+		if len(partial_match) == 1:
+			return partial_match[0]
+		elif len(song_intersection) > 0:
+			return random.sample(list(song_intersection), 1)[0]
+		elif len(partial_match) > 0:
+			return random.sample(partial_match, 1)[0]
+		return None
+
 
 ## Flask アプリのインスタンス化
 app = Flask(__name__)
@@ -58,8 +135,8 @@ def handle_follow(event):
 		replyToken=event.reply_token,
 		messages=[TextMessage(text='Thank You!')]
 	))
-	
-## オウム返しメッセージ
+
+## 返信メッセージ
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
 	## APIインスタンス化
@@ -69,14 +146,14 @@ def handle_message(event):
 	## 受信メッセージの中身を取得
 	received_message = event.message.text
 
-	## APIを呼んで送信者のプロフィール取得
-	profile = line_bot_api.get_profile(event.source.user_id)
-	display_name = profile.display_name
+	## 曲名取得
+	songtitle = search_song_title(received_message)
 
-	## 返信メッセージ編集
-	reply = f'{display_name}さんのメッセージ\n{received_message}'
-
-	## オウム返し
+	## 返信
+	if songtitle != None:
+		reply = 'https://www.youtube.com/watch?v=' + DATA.loc[songtitle, 'official_youtube']
+	else:
+		reply = 'NOT FOUND'
 	line_bot_api.reply_message(ReplyMessageRequest(
 		replyToken=event.reply_token,
 		messages=[TextMessage(text=reply)]
@@ -87,7 +164,7 @@ def handle_message(event):
 def toppage():
 	return 'Hello world!'
 
-## ボット起動コード
+## ボット起動時のコード
 if __name__ == "__main__":
 	## ローカルでテストする時のために、`debug=True` にしておく
 	app.run(host="0.0.0.0", port=8000, debug=True)
